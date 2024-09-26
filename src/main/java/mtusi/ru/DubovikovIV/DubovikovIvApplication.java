013e5826232373c39ae7278b5864ad758c19a174
Vetic16 <IDubovikov@yandex.ru>
2024-09-26 10:34:50 +0300

    initial commit

---
+++ src/main/java/mtusi/ru/DubovikovIV/DubovikovIvApplication.java
@@ -0,0 +1,13 @@
+package mtusi.ru.DubovikovIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubovikovIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubovikovIvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mtusi/ru/DubovikovIV/DubovikovIvApplicationTests.java
@@ -0,0 +1,13 @@
+package mtusi.ru.DubovikovIV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubovikovIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
