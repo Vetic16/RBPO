871dd158c0d91ad727ac9c93607393050bc5a91c
Vetic16 <IDubovikov@yandex.ru>
2024-10-24 16:22:56 +0300

    Delete DubovikovIV directory

--- DubovikovIV/src/main/java/ru/mtuci/Dubovikov/repository/DemoRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mtuci.Dubovikov.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.mtuci.Dubovikov.model.License;
-
-@Repository
-public interface DemoRepository extends JpaRepository<License, Long> {
-    License findByName(String name);
-}
